7edea3038ffbaf572f0f89a4fd4b2ff101482842
Aiel-rgb <bielnevesferrer@gmail.com>
2026-02-21 00:22:29 -0300

    add database migrations for shop item effects

---
+++ drizzle/0015_rainy_micromax.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `shopItems` ADD `hpEffect` int DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE `shopItems` ADD `xpEffect` int DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE `shopItems` ADD `goldEffect` int DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE `shopItems` ADD `effectDescription` text;
